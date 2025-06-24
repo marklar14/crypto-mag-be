a9eecd42b6cd59fadfebc3a1d5b2c11927dd7c3d
Karel Trnka <marklar2010@gmail.com>
2025-06-24 22:14:43 +0200

    fix: auth problems fixed

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,15 +2,20 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule, JwtService } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1h' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
     }),
   ],
-  providers: [AuthService, JwtService],
+  providers: [AuthService],
   controllers: [AuthController],
 })
 export class AuthModule {}
